5a04ecdfb12e28f85f8cd405e5a19add050e8e38
MarioVarano <alexxan@live.it>
2024-07-15 22:22:20 +0200

    responsive

--- Capstone/src/app/app-routing.module.ts
+++ Capstone/src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ const routes: Routes = [
   { path: 'user-profile', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule), canActivate: [AuthGuard, UserGuard] },
   { path: 'professional-profile', loadChildren: () => import('./pages/professional/professional.module').then(m => m.ProfessionalModule), canActivate: [AuthGuard, ProfessionalGuard] },
   { path: 'professionals', loadChildren: () => import('./pages/professionals/professionals.module').then(m => m.ProfessionalsModule) },
-  { path: 'appointment', loadChildren: () => import('./pages/appointment/appointment.module').then(m => m.AppointmentModule) },
   { path: 'faq', loadChildren: () => import('./pages/faq/faq.module').then(m => m.FaqModule) },
   { path: 'confirm-appointment', loadChildren: () => import('./pages/confirm-appointment/confirm-appointment.module').then(m => m.ConfirmAppointmentModule) },
 ];
